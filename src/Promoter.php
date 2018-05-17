08281545e4c88f6633c99f432c021d0697e79f16
Michael Messerli <michaelamesserli@gmail.com>
2018-05-17 20:14:37 +0200

    Add all Tracking methods

--- src/Promoter.php
+++ src/Promoter.php
@@ -35,7 +35,7 @@ class Promoter extends FirstPromoter
      * @return object
      * @throws \Exception
      */
-    public function show($id = null, $cust_id = null, $ref_id = null, $auth_token = null)
+    public static function show($id = null, $cust_id = null, $ref_id = null, $auth_token = null)
     {
         if (empty($id) && empty($cust_id) && empty($ref_id) && empty($auth_token)) {
             throw new Exception('ID, UID, or Email are required.');
@@ -48,8 +48,86 @@ class Promoter extends FirstPromoter
             'auth_token' => $auth_token
         ];
 
-        return $this->request('GET', 'promoters/show', $options);
+        return self::request('GET', 'promoters/show', $options);
+    }
+
+    /**
+     * Modify existing promoter
+     *
+     * @param null $id
+     * @param null $cust_id
+     * @param null $ref_id
+     * @param null $auth_token
+     *
+     * @return object
+     * @throws \Exception
+     *
+     */
+    public static function update($id = null, $cust_id = null, $ref_id = null, $auth_token = null)
+    {
+        if (empty($id) && empty($cust_id) && empty($ref_id) && empty($auth_token)) {
+            throw new Exception('ID, Customer ID, Referral ID, or Auth Token are required.');
+        }
+
+        $options = [
+            'id' => $id,
+            'cust_id' => $cust_id,
+            'ref_id' => $ref_id,
+            'auth_token' => $auth_token
+        ];
+
+        return self::request('PUT', 'promoters/update', $options);
     }
 
 
+    /**
+     * @param null $id
+     * @param null $cust_id
+     * @param null $ref_id
+     * @param null $auth_token
+     *
+     * @return object
+     * @throws \Exception
+     *
+     */
+    public static function delete($id = null, $cust_id = null, $ref_id = null, $auth_token = null)
+    {
+        if (empty($id) && empty($cust_id) && empty($ref_id) && empty($auth_token)) {
+            throw new Exception('ID, Customer ID, Referral ID, or Auth Token are required.');
+        }
+
+        $options = [
+            'id' => $id,
+            'cust_id' => $cust_id,
+            'ref_id' => $ref_id,
+            'auth_token' => $auth_token
+        ];
+
+        return self::request('DELETE', 'promoters/delete', $options);
+    }
+
+    /**
+     * Resetting authentication token(auth_token) which is used to automatically sign in your promoters
+     *
+     * @param null $id
+     * @param null $cust_id
+     * @param null $auth_token
+     *
+     * @return object
+     * @throws \Exception
+     */
+    public static function refreshTracker($id = null, $cust_id = null, $auth_token = null)
+    {
+        if (empty($id) && empty($cust_id) && empty($ref_id) && empty($auth_token)) {
+            throw new Exception('ID, Customer ID, or Auth Token are required.');
+        }
+
+        $options = [
+            'id' => $id,
+            'cust_id' => $cust_id,
+            'auth_token' => $auth_token
+        ];
+
+        return self::request('PUT', 'promoters/refresh_token', $options);
+    }
 }
